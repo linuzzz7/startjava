321c2a626e72c1c69976b5e7da76697f5f0737d1
Alexander Zukov <linuzzz7@yandex.ru>
2021-03-04 20:52:13 +0300

    Пример с массивом

--- src/com/startjava/lesson_2_3_4/array/TestArrays.java
+++ src/com/startjava/lesson_2_3_4/array/TestArrays.java
@@ -1,7 +1,25 @@
 package com.startjava.lesson_2_3_4.array;
 
+import java.util.Scanner;
+
 public class TestArrays {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите длину массива - ");
+        int maxLength = scanner.nextInt();
+        int[] mass = new int[maxLength];
 
+        System.out.print("Заполните массив целыми числами через пробел - ");
+        for (int i = 0; i < maxLength; i++) {
+            mass[i] = scanner.nextInt();
+        }
+
+        int max = mass[0];
+        for (int i = 0; i < maxLength; i++) {
+            if (max < mass[i]) {
+                max = mass[i];
+            }
+        }
+        System.out.println("Максимальное число - " + max);
     }
 }
